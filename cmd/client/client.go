e0ccac79126b320ff88e649e1821d0b1b4e068e0
Roberto Costumero <roberto@costumero.es>
2012-03-20 18:50:37 +0100

    Changes in server and client

--- cmd/client/client.go
+++ cmd/client/client.go
@@ -1,17 +1,26 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"log"
 	"net"
-	"os"
+)
+
+var (
+	verbose *bool = flag.Bool("v", false, "Verbose mode")
+	list *bool = flag.Bool("l", false, "List directories")
 )
 
 func main () {
 	conn, err := net.Dial("tcp", "localhost:5453")
 
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error: %s\n", err)
+		log.Fatalf("Error: %s\n", err)
 	}
 
+	if *list {
+		fmt.Fprintf(conn, "List\n")
+	}
 	conn.Close()
 }
--- cmd/server/server.go
+++ cmd/server/server.go
@@ -1,34 +1,82 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"log"
 	"net"
+	"os"
+	"path/filepath"
 )
 
 var (
+	// Flags
 	verbose *bool = flag.Bool("v", false, "Verbose output ON")
 	cosmofsin *string = flag.String("cosmofsin", os.Getenv("COSMOFSIN"), "Location of incoming packages")
 	cosmofsout *string = flag.String("cosmofsout", os.Getenv("COSMOFSOUT"), "Location of shared directories")
+
+	// Shared Directory List
+	sharedDirList []string = make([]string, len(filepath.SplitList(*cosmofsout)))
 )
 
+// Handles petitions from the peers.
 func handlePetition (conn net.Conn) {
 	if *verbose {
 		log.Printf("Connection made from: %s\n", conn.RemoteAddr())
 	}
+
+
+	reader := bufio.NewReader(conn)
+
+	line, err := reader.ReadString('\n')
+
+	if err != nil {
+		log.Printf("Error reading connection: %s", err)
+	}
+
+	log.Println(line)
 }
 
 func main () {
 	flag.Parse()
 
+	// Check if COSMOFSIN environment is set
 	if *cosmofsin == "" {
 		log.Fatalf("COSMOFSIN not set correctly. Current content <%s>", *cosmofsin)
 	}
 
+	// Check if COSMOFSIN is a correct directory
 	if _, err := os.Lstat(*cosmofsin); err != nil {
 		log.Fatalf("COSMOFSIN not set correctly. Current content <%s>", *cosmofsin)
 	}
 
+	if *verbose {
+		log.Printf("Inbound files arriving to %s", *cosmofsin)
+	}
+
+	// Initialize sharedDirList
+	sharedDirList = filepath.SplitList(*cosmofsout)
+
+	// There shall be at least one shared directory
+	if len(sharedDirList) == 0 {
+		log.Fatal("COSMOFSOUT should have at least one directory or file.")
+	}
+
+	for _, dir := range sharedDirList {
+		log.Println(filepath.Clean(dir))
+
+		_, err := os.Lstat(filepath.Join(filepath.Clean(dir), ".cosmofs/config"))
+
+		if err != nil {
+			log.Printf("Error reading file: %s", err)
+			// Create the config file.
+			// Read the directory and include the files on it.
+		}
+
+		// Decode the config file and update data structures.
+	}
+
+	// Leave the process listening for other peers
 	ln, err := net.Listen("tcp", ":5453")
 
 	if err != nil {
