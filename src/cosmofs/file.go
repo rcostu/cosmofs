352c093bab7b35e9a3c60cc710d2dde4237f8464
Roberto Costumero <roberto@costumero.es>
2012-03-21 20:52:20 +0100

    Made Chunks fields exported to work with gob encoding system

---
+++ src/cosmofs/file.go
@@ -0,0 +1,24 @@
+package cosmofs
+
+type chunk struct {
+	Name string
+	RemPath string
+	//peer peer.Peer
+}
+
+type File struct {
+	Path string
+	Filename string
+	Chunks []chunk
+	NumChunks bool
+	MaintainChunks bool
+	Online bool
+	KeepCopy bool
+	Public bool
+	Backup bool
+}
+
+type Directory struct {
+	File
+	Recursive bool
+}
