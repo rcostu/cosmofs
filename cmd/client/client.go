69860633c661ea588af5f92fc240ac5b6433533f
Roberto Costumero <roberto@costumero.es>
2012-04-26 10:54:09 +0200

    Changes in server-client communication to start future peer discovery

--- cmd/client/client.go
+++ cmd/client/client.go
@@ -1,12 +1,36 @@
+/**
+
+Copyright (C) 2012  Roberto Costumero Moreno <roberto@costumero.es>
+
+This file is part of Cosmofs.
+
+Cosmofs is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Cosmofs is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Cosmofs.  If not, see <http://www.gnu.org/licenses/>.
+
+**/
+
 package main
 
 import (
+	//ssh "code.google.com/p/go.crypto/ssh"
 	"cosmofs"
 	"encoding/gob"
 	"flag"
 	"fmt"
 	"log"
 	"net"
+	"os"
+	"path/filepath"
 )
 
 var (
@@ -25,6 +49,34 @@ func debug (format string, v ...interface{}) {
 	}
 }
 
+func parseKey() {
+	keyFileName := filepath.Join(os.Getenv("HOME"), ".ssh", "id_rsa.pub")
+
+	fi, err := os.Lstat(keyFileName)
+
+	if err != nil {
+		log.Fatal("Error: SSH Key file doesn't found.")
+	}
+
+	keyFile, err := os.Open(keyFileName)
+
+	if err != nil {
+		log.Fatal("Error: Cannot open SSH Key file.")
+	}
+
+	defer keyFile.Close()
+
+	buffer := make([]byte, fi.Size())
+
+	keyFile.Read(buffer)
+
+	fmt.Printf("%s\n", buffer)
+
+	//var agKey *ssh.AgentKey = &ssh.AgentKey{blob: buffer, Comment: ""}
+
+	//fmt.Println(agKey.Key())
+}
+
 func main () {
 	flag.Parse()
 
@@ -32,12 +84,22 @@ func main () {
 		log.Fatal("A server must be specified")
 	}
 
-	conn, err := net.Dial("tcp", flag.Arg(0) + ":" + PORT)
-	defer conn.Close()
+	//parseKey()
+
+	//conn, err := net.Dial("tcp", flag.Arg(0) + ":" + PORT)
+
+	conn, err := net.DialTCP("tcp", nil, &net.TCPAddr{
+		IP:		net.IPv4(10,0,0,255),
+		Port:	5453,
+	})
 
 	if err != nil {
 		log.Fatalf("Error: %s\n", err)
 	}
+
+	fmt.Println(conn.RemoteAddr())
+
+	defer conn.Close()
 
 	var decodedFiles map[string] []*cosmofs.File = make(map[string] []*cosmofs.File)
 	configDec := gob.NewDecoder(conn)
--- cmd/server/server.go
+++ cmd/server/server.go
@@ -1,3 +1,24 @@
+/**
+
+Copyright (C) 2012  Roberto Costumero Moreno <roberto@costumero.es>
+
+This file is part of Cosmofs.
+
+Cosmofs is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Cosmofs is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Cosmofs.  If not, see <http://www.gnu.org/licenses/>.
+
+**/
+
 package main
 
 import (
@@ -243,7 +264,10 @@ func main () {
 	}
 
 	// Leave the process listening for other peers
-	ln, err := net.Listen("tcp", ":5453")
+	ln, err := net.ListenTCP("tcp", &net.TCPAddr{
+		IP:		net.IPv4zero,
+		Port:	5453,
+	})
 
 	if err != nil {
 		debug("Error: %s\n", err)
@@ -252,7 +276,7 @@ func main () {
 	debug("Listening on address ", ln.Addr())
 
 	for {
-		conn, err := ln.Accept()
+		conn, err := ln.AcceptTCP()
 		if err != nil {
 			debug("Error: %s\n", err)
 			continue
